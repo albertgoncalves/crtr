8c9e364b759cc15098222a38f31f6d3a64c0519a
albertgoncalves <goncalves.albert@gmail.com>
2020-05-04 20:00:53 -0400

    some stuff

--- src/main.c
+++ src/main.c
@@ -14,6 +14,15 @@
 #define WIDTH_FLOAT  1024.0f
 #define HEIGHT_FLOAT 1024.0f
 
+#define FILM_DISTANCE 1.0f
+#define FILM_WIDTH    1.0f
+#define FILM_HEIGHT   1.0f
+
+#define EPSILON 0.001f
+
+#define N_PLANES  1
+#define N_SPHERES 1
+
 #define FILEPATH "out/main.bmp"
 
 #include "bmp.h"
@@ -30,22 +39,19 @@ typedef struct {
     rgbColor color;
 } sphere;
 
-#define EPSILON 0.0001f
-
-#define N_PLANES  1
-#define N_SPHERES 1
+static vec3 CAMERA_POSITION = {.x = 0.0f, .y = 10.0f, .z = 1.0f};
 
 static rgbColor BACKGROUND = {.red = 240, .green = 240, .blue = 240};
 
 static plane PLANES[N_PLANES] = {
     {.normal = {.x = 0.0f, .y = 0.0f, .z = 1.0f},
-     .delta = 0.0f,
+     .delta = 1.0f,
      .color = {.red = 255, .green = 0, .blue = 255}},
 };
 
 static sphere SPHERES[N_SPHERES] = {
     {.center = {.x = 0.5f, .y = -2.0f, .z = 1.25f},
-     .radius = 2.0f,
+     .radius = 3.0f,
      .color = {.red = 0, .green = 255, .blue = 255}},
 };
 
@@ -72,9 +78,11 @@ static rgbColor cast_ray(vec3 ray_origin, vec3 ray_direction) {
         f32    denom = 2.0f * a;
         f32    root = sqrtf((b * b) - (4.0f * a * c));
         if (EPSILON < root) {
-            f32 t_pos = (-b + root) / denom;
-            f32 t_neg = (-b - root) / denom;
-            f32 t = ((0.0f < t_neg) && (t_neg < t_pos)) ? t_neg : t_pos;
+            f32 t_positive = (-b + root) / denom;
+            f32 t_negative = (-b - root) / denom;
+            f32 t = ((0.0f < t_negative) && (t_negative < t_positive))
+                        ? t_negative
+                        : t_positive;
             if ((0.0f < t) & (t < threshold)) {
                 threshold = t;
                 color = s.color;
@@ -85,28 +93,25 @@ static rgbColor cast_ray(vec3 ray_origin, vec3 ray_direction) {
 }
 
 static void render(pixel* pixels) {
-    vec3 camera_pos = {.x = 0.0f, .y = 10.0f, .z = 1.0f};
-    vec3 camera_z = normalize_vec3(camera_pos);
+    vec3 camera_z = normalize_vec3(CAMERA_POSITION);
     vec3 camera_x =
         normalize_vec3(cross_vec3(new_vec3(0.0f, 0.0f, 1.0f), camera_z));
     vec3 camera_y = normalize_vec3(cross_vec3(camera_z, camera_x));
-    f32  film_dist = 1.0f;
-    f32  film_width = 1.0f;
-    f32  film_height = 1.0f;
-    f32  film_half_width = 0.5f * film_width;
-    f32  film_half_height = 0.5f * film_height;
-    vec3 film_center = sub_vec3(camera_pos, mul_vec3_f32(camera_z, film_dist));
+    vec3 film_center =
+        sub_vec3(CAMERA_POSITION, mul_vec3_f32(camera_z, FILM_DISTANCE));
+    f32 film_half_width = 0.5f * FILM_WIDTH;
+    f32 film_half_height = 0.5f * FILM_HEIGHT;
     for (u32 y = 0; y < HEIGHT; ++y) {
         f32 film_y = (2.0f * ((f32)y / HEIGHT_FLOAT)) - 1.0f;
         for (u32 x = 0; x < WIDTH; ++x) {
             f32  film_x = (2.0f * ((f32)x / WIDTH_FLOAT)) - 1.0f;
-            vec3 film_pos = add_vec3(
+            vec3 film_position = add_vec3(
                 add_vec3(film_center,
                          mul_vec3_f32(camera_x, film_x * film_half_width)),
                 mul_vec3_f32(camera_y, film_y * film_half_height));
-            rgbColor color =
-                cast_ray(camera_pos,
-                         normalize_vec3(sub_vec3(film_pos, camera_pos)));
+            rgbColor color = cast_ray(
+                CAMERA_POSITION,
+                normalize_vec3(sub_vec3(film_position, CAMERA_POSITION)));
             pixels->red = color.red;
             pixels->green = color.green;
             pixels->blue = color.blue;
