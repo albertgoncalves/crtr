e343060ec9ea41e454f4ac73a8ba9ca55b4bb318
Albert Goncalves <goncalves.albert@gmail.com>
2020-05-10 00:29:47 -0400

    remove unused stuff

--- src/math.h
+++ src/math.h
@@ -13,21 +13,8 @@ static u8 saturate_add_u8(u8 a, u8 b) {
     return (U8_MAX - b) < a ? (u8)U8_MAX : (u8)(a + b);
 }
 
-static u8 saturate_sub_u8(u8 a, u8 b) {
-    return a <= b ? (u8)0 : (u8)(a - b);
-}
-
 static f32 dot_vec3(vec3 a, vec3 b) {
     return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
-}
-
-static vec3 cross_vec3(vec3 a, vec3 b) {
-    vec3 result = {
-        .x = (a.y * b.z) - (a.z * b.y),
-        .y = (a.z * b.x) - (a.x * b.z),
-        .z = (a.x * b.y) - (a.y * b.x),
-    };
-    return result;
 }
 
 static vec3 mul_vec3_f32(vec3 a, f32 b) {
@@ -59,16 +46,6 @@ static vec3 sub_vec3(vec3 a, vec3 b) {
 
 static f32 len_vec3(vec3 a) {
     return sqrtf((a.x * a.x) + (a.y * a.y) + (a.z * a.z));
-}
-
-static vec3 normalize_vec3(vec3 a) {
-    f32  length = sqrtf((a.x * a.x) + (a.y * a.y) + (a.z * a.z));
-    vec3 result = {
-        .x = a.x / length,
-        .y = a.y / length,
-        .z = a.z / length,
-    };
-    return result;
 }
 
 #endif
